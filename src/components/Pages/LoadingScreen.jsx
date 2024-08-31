a7190d055186826f8365550faf8c510a3c6037af
Muhammed Farseen T P <muhdfarseen@gmail.com>
2024-08-31 17:24:19 +0530

    Update LoadingScreen.jsx

--- src/components/Pages/LoadingScreen.jsx
+++ src/components/Pages/LoadingScreen.jsx
@@ -3,7 +3,7 @@ import { Flex, Spinner } from '@radix-ui/themes';
 export const LoadingScreen = () => {
     return (
         <div>
-            <Flex height={'100vh'} align={'center'} justify={'between'}>
+            <Flex height={'100vh'} align={'center'} justify={'center'}>
                 <Spinner />
             </Flex>
         </div>
